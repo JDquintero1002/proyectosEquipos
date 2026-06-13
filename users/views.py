d8099295b7052d123412894ff93a3b19f6748151
JDquintero1002 <jdquintero@unicauca.edu.co>
2026-06-12 21:10:18 -0500

    ajustes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from users.api.serializers import UserSerializer
+from users.models import User 
+from django.contrib.auth.hashers import make_password
 # Create your views here.
+
+class UserApiWieset(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.object.all()
+    def create(self, request, *args, **kwargs):
+        request.data('password') = make_password
+        return super().create(request, *args,**kwargs)
